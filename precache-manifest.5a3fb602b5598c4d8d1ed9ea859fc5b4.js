e41eb7bd5b65068067e1d4666c5ba75595e63072
Evgeny Cherkasov <StipJey@gmail.com>
2020-04-19 16:26:55 +0300

    Updates

---
+++ precache-manifest.5a3fb602b5598c4d8d1ed9ea859fc5b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89dbe497a81e3723def1929f79092f79",
+    "url": "/tlroadmap-ui/index.html"
+  },
+  {
+    "revision": "a41c6f147164b42a80e2",
+    "url": "/tlroadmap-ui/static/css/main.4ec0e698.chunk.css"
+  },
+  {
+    "revision": "db8750be8f6d977f27fd",
+    "url": "/tlroadmap-ui/static/js/2.1eb89927.chunk.js"
+  },
+  {
+    "revision": "a41c6f147164b42a80e2",
+    "url": "/tlroadmap-ui/static/js/main.cbbc4de6.chunk.js"
+  },
+  {
+    "revision": "1d29b8c20d5b5822a210",
+    "url": "/tlroadmap-ui/static/js/runtime~main.ce638a69.js"
+  }
+]);
